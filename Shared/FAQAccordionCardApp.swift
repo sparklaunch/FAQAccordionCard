c4e83d0b09cff56aa16a91202c886742cf1f0f9e
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-26 17:44:01 +0900

    Initial Commit

---
+++ Shared/FAQAccordionCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAQAccordionCardApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FAQAccordionCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
