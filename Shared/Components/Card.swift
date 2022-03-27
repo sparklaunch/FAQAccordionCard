2dd39befd0db3411e79880c32d31a43edee044ff
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-27 12:26:38 +0900

    Add the card.

--- Shared/Components/Card.swift
+++ Shared/Components/Card.swift
@@ -9,6 +9,16 @@ import SwiftUI
 
 struct Card: View {
     var body: some View {
-        
+        ZStack {
+            Color.white
+            Text("FAQ")
+                .font(.custom("Kumbh Sans", size: 32))
+                .fontWeight(.bold)
+                .foregroundColor(Color("TitleColor"))
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .padding(.horizontal, 30)
+        .scaledToFit()
+        .shadow(radius: 5)
     }
 }
